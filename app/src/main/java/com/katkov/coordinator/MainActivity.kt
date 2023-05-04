4b736b1d8f2f3d1fa1beb527ca62576fba957766
Michael Katkov <eloorus@gmail.com>
2023-05-04 23:35:31 +0200

    Example refactoring

--- app/src/main/java/com/katkov/coordinator/MainActivity.kt
+++ app/src/main/java/com/katkov/coordinator/MainActivity.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.katkov.coordinator.ui.theme.CoordinatorTheme
 import com.katkov.coordinatorlayout.*
@@ -39,43 +38,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun AppBarLayoutExample(toolbarState: ToolbarState) {
+    val context = LocalContext.current
+    AppBarLayout(
+        title = "App Bar",
+        toolbarState = toolbarState,
+        background = { Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "",
+            contentScale = ContentScale.FillBounds
+        ) },
+        navigationIcon = { IconButton(onClick = {
+            Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+        }) {
+            Icon(
+                imageVector = Icons.Filled.Menu,
+                contentDescription = null,
+                tint = Color.Black
+            )
+        } },
+        actions = { IconButton(onClick = {
+            Toast.makeText(context, "Button2 Clicked", Toast.LENGTH_LONG).show()
+        }) {
+            Icon(
+                imageVector = Icons.Filled.Star,
+                contentDescription = null,
+                tint = Color.Black
+            )
+        }})
+}
+
+@Composable
 fun BodyWithEnterAlwaysCollapsedBehavior() {
     val density = LocalDensity.current
-    val context = LocalContext.current
     val minToolbarHeightPx = with(density) { 56.dp.roundToPx().toFloat() }
     val maxToolbarHeightPx = with(density) { 220.dp.roundToPx().toFloat() }
     val toolbarState = rememberToolBarState(minToolbarHeightPx, maxToolbarHeightPx)
     CoordinatorLayout(
         headerElevation = 1.dp,
-        behavior = ExitUntilCollapsedBehavior(toolbarState),
+        behavior = EnterAlwaysCollapsedBehavior(toolbarState),
         toolbarContent = {
-            AppBarLayout(
-                title = "App Bar",
-                toolbarState = toolbarState,
-                background = { Image(
-                    modifier = Modifier.fillMaxSize(),
-                    painter = painterResource(id = R.drawable.ic_launcher_background),
-                    contentDescription = "",
-                    contentScale = ContentScale.FillBounds
-                ) },
-                navigationIcon = { IconButton(onClick = {
-                    Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
-                }) {
-                    Icon(
-                        imageVector = Icons.Filled.Menu,
-                        contentDescription = null,
-                        tint = Color.Black
-                    )
-                } },
-                actions = { IconButton(onClick = {
-                    Toast.makeText(context, "Button2 Clicked", Toast.LENGTH_LONG).show()
-                }) {
-                    Icon(
-                        imageVector = Icons.Filled.Star,
-                        contentDescription = null,
-                        tint = Color.Black
-                    )
-                }})
+            AppBarLayoutExample(toolbarState = toolbarState)
         }) {
         items(100) { index ->
             Text(modifier = Modifier
@@ -85,18 +89,5 @@ fun BodyWithEnterAlwaysCollapsedBehavior() {
                 color = Color.Black)
             Divider(modifier = Modifier.height(1.dp), color = Color.Black)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CoordinatorTheme {
-        Greeting("Android")
     }
 }
